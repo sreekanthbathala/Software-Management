001a44d158fe8a43086b53115d9d2542bbc11de4
Sreekanth <sreekanthbathala8@gmail.com>
2026-06-18 14:26:27 +0530

    Aadded Nested Serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User, Project, Task, Training, LeaveRequest
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -24,21 +25,48 @@ class ProjectSerializer(serializers.ModelSerializer):
         model = Project
         fields = ['id', 'name', 'description', 'start_date', 'end_date', 'manager', 'status']
 
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        if instance.manager:
+            response['manager'] = UserSerializer(instance.manager).data
+        return response
+
 class TaskSerializer(serializers.ModelSerializer):
-    created_by = serializers.ReadOnlyField(source='created_by.username')
-    assigned_to = serializers.ReadOnlyField(source='assigned_to.username')
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'project', 'assigned_to', 'due_date', 'status', 'created_by']
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        if instance.project:
+            response['project'] = ProjectSerializer(instance.project).data
+        if instance.created_by:
+            response['created_by'] = UserSerializer(instance.created_by).data
+        if instance.assigned_to:
+            response['assigned_to'] = UserSerializer(instance.assigned_to).data
+        return response
 
 class TrainingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Training
         fields = ['id', 'title', 'description', 'instructor', 'date', 'attendees']
 
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        if instance.instructor:
+            response['instructor'] = UserSerializer(instance.instructor).data
+        response['attendees'] = UserSerializer(instance.attendees.all(), many=True).data
+        return response
+
 
 class LeaveRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeaveRequest
         fields = ['id', 'employee', 'start_date', 'end_date', 'reason', 'status']
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        if instance.employee:
+            response['employee'] = UserSerializer(instance.employee).data
+        return response
 
