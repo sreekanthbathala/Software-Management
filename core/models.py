d7933fc6b11bed840c1dc640faae43f55d45c4e8
Sreekanth <sreekanthbathala8@gmail.com>
2026-06-22 10:19:49 +0530

    changed

--- core/models.py
+++ core/models.py
@@ -50,14 +50,7 @@ class Task(models.Model):
     )
     status = models.CharField(max_length=20, choices=choice, default='Pending')
 
-    PRIORITY_CHOICES = (
-        ('Low', 'Low'),
-        ('Medium', 'Medium'),
-        ('High', 'High'),
-        ('Critical', 'Critical')
-    )
-    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='Medium')
-
+   
     def __str__(self):
         return self.title
 
